f76a9263139a8f24555d406a33332b7032fee9c8
MrPipson <brkuljan@gmail.com>
2018-04-21 16:55:12 +0200

    some changes

--- 5 - More Types/5 - List Slices.py
+++ 5 - More Types/5 - List Slices.py
@@ -29,3 +29,23 @@ print(" [7:]")
 print(squares[7:])
 
 # Slicing can also be done on tuples.
+
+# List slices can also have a third number, representing the step,
+# to include only alternate values in the slice.
+
+print("\n\n Third STEP parameter can be used")
+print(squares[::2])
+print(squares[2:8:3])
+print(squares[::1])
+
+# [2:8:3] will include elements starting from the 2nd index up to the 8th with a step of 3
+# Slice step cannot be zero.
+
+# Negative values can be used in list slicing (and normal list indexing).
+# When negative values are used for the first and second values in a slice
+# (or a normal index), they count from the end of the list.
+
+print("\n\n Using negative indexing")
+print(squares[1:-1]) #goes to the penultimet element
+print(squares [::-1]) #slice is done backwards. used to reverse the list idiomatically
+print(squares[7:5:-1]) #when using negative step even start and end element are reversed
